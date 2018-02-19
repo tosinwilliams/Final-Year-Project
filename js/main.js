e70deb3ee9a66c43f3eb3ffb79efc67294e167e0
tosinwilliams <tosin_williams@outlook.com>
2018-02-19 18:35:44 +0000

    Added interactive on-click art posters via JS cursor listener

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ window.onload = function () {
     };
 };
 
-
+// Component to change to a sequential image on click.
+AFRAME.registerComponent('cursor-listener', {
+  init: function () {
+    var lastIndex = -1;
+    var IMAGES = ['images/art1.jpg','images/art2.jpg','images/art3.jpg','images/art4.jpg','images/art5.jpg',];
+    this.el.addEventListener('click', function (evt) {
+      lastIndex = (lastIndex + 1) % IMAGES.length;
+      this.setAttribute('material', 'src', IMAGES[lastIndex]);
+      console.log('I was clicked at: ', evt.detail.intersection.point);
+    });
+  }
+});
