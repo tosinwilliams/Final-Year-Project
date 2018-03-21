e8e4767df1efb754a85011605e7387b54eaf3098
tosinwilliams <tosin_williams@outlook.com>
2018-03-21 00:05:11 +0000

    Defined motion path animation for model spaceship and added home/main menu links to mobile and desktop VR scenes

--- js/main.js
+++ js/main.js
@@ -9,38 +9,30 @@ window.onload = function () {
 
 // Component to change to a sequential image on click.
 AFRAME.registerComponent('cursor-listener', {
-  init: function () {
-    var lastIndex = -1;
-    var IMAGES = ['images/art1.jpg','images/art2.jpg','images/art3.jpg','images/art4.jpg','images/art5.jpg',];
-    this.el.addEventListener('click', function (evt) {
-      lastIndex = (lastIndex + 1) % IMAGES.length;
-      this.setAttribute('material', 'src', IMAGES[lastIndex]);
-      console.log('I was clicked at: ', evt.detail.intersection.point);
-    });
-  }
-});
-
-//// Component to activate spaceship animation on click
-//AFRAME.registerComponent('cursor-animator', {
-//  init: function () {
-//    this.el.addEventListener('click', function (evt) {
-//        //Code
-//      console.log('I was clicked at: ', evt.detail.intersection.point);
-//    });
-//  }
-//});
-
-    AFRAME.registerComponent('cursor-animator', {
-      init: function () {
+    init: function () {
+        var lastIndex = -1;
+        var IMAGES = ['images/art1.jpg', 'images/art2.jpg', 'images/art3.jpg', 'images/art4.jpg', 'images/art5.jpg', ];
         this.el.addEventListener('click', function (evt) {
-          console.log('click')
+            lastIndex = (lastIndex + 1) % IMAGES.length;
+            this.setAttribute('material', 'src', IMAGES[lastIndex]);
+            console.log('I was clicked at: ', evt.detail.intersection.point);
         });
-      }
-    });
+    }
+});
 
 //Component to automatically persist in vr
 AFRAME.registerComponent('auto-enter-vr', {
-  init: function () {
-    this.el.sceneEl.enterVR();
-  }
+    init: function () {
+        this.el.sceneEl.enterVR();
+    }
+});
+
+AFRAME.registerComponent('raycaster-autorefresh', {
+    init: function () {
+        var el = this.el;
+        this.el.addEventListener('model-loaded', function () {
+            var cursorEl = el.querySelector('[raycaster]');
+            cursorEl.components.raycaster.refreshObjects();
+        });
+    }
 });
